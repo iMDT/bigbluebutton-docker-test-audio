f7efb54bdc96b99f2f5ab9881b12379787261393
Tiago Jacobs <tiago@imdt.com.br>
2021-07-23 11:18:32 -0300

    Update run-debug.sh

--- run-debug.sh
+++ run-debug.sh
@@ -10,6 +10,6 @@ rm -rf ./debug/ &> /dev/null || true
 mkdir -m  777 debug &> /dev/null || true
 mkdir -m  777 /tmp/debug$$ &> /dev/null || true
 
-docker run -v /tmp/debug$$:/debug -it bbb-docker-test-audio "${JOIN_URL}" && echo 1 || echo 0
+docker run -v /tmp/debug$$:/debug -it bigbluebutton-docker-test-audio "${JOIN_URL}" && echo 1 || echo 0
 
 mv /tmp/debug$$ ./debug/
